ec68142a5a3b46017ed8720a7578bbc411662f47
Natan Amorim <39222731+NatanAmorim@users.noreply.github.com>
2019-10-15 17:22:23 -0300

    Update basics.sh

--- GNU+Linux/ubuntu/basics.sh
+++ GNU+Linux/ubuntu/basics.sh
@@ -6,13 +6,13 @@
 sudo apt update && sudo apt upgrade
 
 # Basic applications
-sudo apt install wget curl htop neofetch
+sudo apt install wget curl htop
 
 # Compilers & Interpreters
 sudo apt install gcc make cmake build-essential autoconf automake python3-minimal
 
 # Terminal workflow
-sudo apt install tmux vim git zsh tldr-py
+sudo apt install tmux vim git zsh tldr-py lsd
 
 # File compression
 sudo apt install tar zip gzip unzip lrzip unrar p7zip unace
@@ -24,8 +24,8 @@ sudo apt install breeze-cursor-theme breeze-icon-theme
 sudo apt install fonts-ubuntu fonts-roboto
 sudo apt install fonts-hack fonts-poweline fonts-firacode
 
-# Others
-sudo apt install gparted firefox chromium-browser spyder3 aptitude vifm ranger 
+# Java
+sudo apt install openjdk-11-jdk openjfx
 
-# Alternative to file-roller
-#sudo apt install ark
+# Others
+sudo apt install gparted firefox chromium-browser spyder3 aptitude vifm ranger neofetch
