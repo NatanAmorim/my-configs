08aab76793b978634f09bef957bec788e6aba81c
Natan Amorim <39222731+NatanAmorim@users.noreply.github.com>
2019-11-22 14:15:51 -0300

    Update setup.sh
    midnight-commander not working

--- Windows/wsl-debian-setup/setup.sh
+++ Windows/wsl-debian-setup/setup.sh
@@ -12,7 +12,7 @@ sudo apt install wget curl neofetch
 sudo apt install gcc make cmake build-essential autoconf automake python3-minimal
 
 # Terminal workflow
-sudo apt install vim git zsh tmux tldr-py midnight-commander vifm aptitude ranger
+sudo apt install vim git zsh tmux tldr-py vifm aptitude ranger
 
 # File compression
 sudo apt install tar zip gzip unzip lrzip unrar p7zip unace
