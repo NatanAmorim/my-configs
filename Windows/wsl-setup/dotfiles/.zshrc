900d740a171b87328df2b9f29dd7fbd00d06de54
Natan Amorim <natanamorim@users.noreply.github.com>
2020-08-13 12:45:05 -0300

    Update files for wsl 2

---
+++ Windows/wsl-setup/dotfiles/.zshrc
@@ -0,0 +1,46 @@
+export ZSH="/home/nate/.oh-my-zsh"
+export UPDATE_ZSH_DAYS=7
+
+ZSH_THEME="zhann"
+CASE_SENSITIVE="false"
+HYPHEN_INSENSITIVE="true"
+DISABLE_AUTO_UPDATE="false"
+DISABLE_AUTO_TITLE="true"
+ENABLE_CORRECTION="true"
+HIST_STAMPS="dd/mm/yyyy"
+COMPLETION_WAITING_DOTS="true"
+
+# Would you like to use another custom folder than $ZSH/custom?
+# ZSH_CUSTOM=/path/to/new-custom-folder
+
+plugins=(
+sudo
+extract
+github
+docker  
+colored-man-pages
+python
+yarn
+zsh-syntax-highlighting
+)
+
+source $ZSH/oh-my-zsh.sh
+
+# User configuration
+
+export LANG=pt_BR.UTF-8
+export TERM=xterm-256color
+
+# Load personal aliases, for a full list of active aliases, run `alias`
+if [ -f $HOME/.aliases ]; then
+. ~/.aliases
+fi
+
+# Print a newline before the prompt, unless it's the first prompt in the process.
+function precmd() {
+    if [ -z "$NEW_LINE_BEFORE_PROMPT" ]; then
+        NEW_LINE_BEFORE_PROMPT=1
+    elif [ "$NEW_LINE_BEFORE_PROMPT" -eq 1 ]; then
+        echo ""
+    fi
+}
--- Windows/wsl-setup/ubuntu-setup.sh
+++ Windows/wsl-setup/ubuntu-setup.sh
@@ -1,33 +1,25 @@
 #!/bin/bash
 # basics.sh
-
 # You may need to "sudo chmod +x basics.sh" 
-
 sudo apt update && sudo apt upgrade
-
 # Basic applications
 sudo apt install htop wget curl
-
 # Compilers & Interpreters
 sudo apt install gcc make cmake build-essential autoconf automake python3-minimal
-
 # Terminal workflow
 sudo apt install vim git zsh tmux tldr-py vifm aptitude ranger neofetch lolcat
-
 # File compression
 sudo apt install tar zip gzip unzip lrzip p7zip unrar-free
-
 # Java
 sudo apt install openjdk-11-jdk
-
 # Vim Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
 # zsh config
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" # oh-my-zsh
-chsh -s $(which zsh) # change default zsh
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting # Syntax Highlights plugin
-
+## oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" 
+## change default zsh
+chsh -s $(which zsh) 
+## Syntax Highlights plugin
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 # Cleaning
 sudo apt autoremove
-
