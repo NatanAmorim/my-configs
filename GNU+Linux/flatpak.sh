b27280ef45376bee9d501e7cadce5d1246ae6fd3
Natan Amorim <39222731+NatanAmorim@users.noreply.github.com>
2020-12-09 01:41:21 -0300

    Update flatpak.sh

--- GNU+Linux/flatpak.sh
+++ GNU+Linux/flatpak.sh
@@ -1,38 +1,51 @@
 #!/bin/bash
-# flatpak.sh
-
-# You may need to "sudo chmod +x flatpak.sh" 
 
 # Office & Documentation
- flatpak install flathub com.microsoft.Teams # (Microsoft Teams for Linux)
- flatpak install flathub org.gnome.Evince # Evince (PDF document viewer)
- flatpak install flathub org.libreoffice.LibreOffice # LibreOffice
- flatpak install flathub org.remmina.Remmina # Remmina
+ flatpak install flathub org.kde.okular # Okular (PDF document viewer)
 
 # Programming & IDE's (Integrated Development Environments)
- flatpak install flathub org.godotengine.Godot # Godot (Game Engine)
  flatpak install flathub com.jetbrains.IntelliJ-IDEA-Community # IntelliJ-IDEA
- flatpak install flathub com.jetbrains.PyCharm-Community # Pycharm
- flatpak install flathub cc.arduino.arduinoide # Arduino IDE
+ flatpak install flathub com.github.artemanufrij.regextester # Regex Tester (test regular expressions)
 
 # Other purpose tools
  flatpak install flathub org.filezillaproject.Filezilla # Filezilla
 
 # Graphics & Photography
- flatpak install flathub org.darktable.Darktable # Darktable
  flatpak install flathub org.inkscape.Inkscape # Inkscape
  flatpak install flathub org.gimp.GIMP # GIMP
- flatpak install flathub org.kde.krita # Kirita
 
 # Audio & Video
  flatpak install flathub org.kde.kdenlive # Kdenlive
  flatpak install flathub org.audacityteam.Audacity # Audacity
  flatpak install flathub com.obsproject.Studio # OBS - Open Broadcaster Software
- flatpak install flathub io.lmms.LMMS # LMMS - A music production application
 
-# 2D & 3D animation software
- flatpak install flathub com.github.libresprite.LibreSprite # LibreSprite
- flatpak install flathub org.synfig.SynfigStudio # SynfigStudio
- flatpak install flathub io.github.OpenToonz # OpenToonz
- flatpak install flathub org.pencil2d.Pencil2D # Pencil2D
- flatpak install flathub org.blender.Blender # Blender
+## Good programs that i don't use
+
+# flatpak install flathub io.lmms.LMMS # LMMS (A music production application)
+
+# flatpak install flathub com.github.libresprite.LibreSprite # LibreSprite (Animated sprite editor and pixel art tool)
+
+# flatpak install flathub org.synfig.SynfigStudio # SynfigStudio (2D animations and compositions)
+
+# flatpak install flathub io.github.OpenToonz # OpenToonz (2D animations)
+
+# flatpak install flathub org.pencil2d.Pencil2D # Pencil2D (2D animations)
+
+# flatpak install flathub org.blender.Blender # Blender (3D animations)
+
+# flatpak install flathub org.kde.krita # Kirita (Digital Painting)
+
+# flatpak install flathub org.remmina.Remmina # Remmina (Remote Desktop)
+
+# flatpak install flathub org.libreoffice.LibreOffice # LibreOffice (Office Suite)
+
+# flatpak install flathub org.darktable.Darktable # Darktable (Raw Photo editor)
+
+# flatpak install flathub org.godotengine.Godot # Godot (Game Engine)
+
+# flatpak install flathub ch.openboard.OpenBoard # OpenBoard (Interactive whiteboard)
+
+## I don't know if it is safe yet
+# flatpak install flathub com.ulduzsoft.Birdtray
+# flatpak install flathub im.gitter.Gitter
+# flatpak install flathub com.gigitux.gtkwhats
