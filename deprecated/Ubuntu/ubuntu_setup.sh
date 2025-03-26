13753dd78df7bfa64773d046764a6b6b569c44df
Natan Amorim <39222731+NatanAmorim@users.noreply.github.com>
2025-03-26 15:40:53 -0300

    Update ubuntu_setup.sh
    Trying Ubuntu again

--- deprecated/Ubuntu/ubuntu_setup.sh
+++ deprecated/Ubuntu/ubuntu_setup.sh
@@ -4,20 +4,17 @@
 # You may need to "sudo chmod +x ubuntu_setup.sh"
 
 sudo apt update && sudo apt upgrade
-# Basic applications
-sudo apt install wget curl htop
+# Terminal workflow
+sudo apt install wget curl git zsh htop neovim
 # Compilers & Interpreters
 sudo apt install gcc make cmake build-essential autoconf automake
-# Terminal workflow
-sudo apt install neovim git zsh tldr-py neofetch ncdu ripgrep
 # File compression
-sudo apt install tar zip gzip unzip lrzip unrar p7zip-full unace
+sudo apt install tar zip gzip unzip lrzip unrar p7zip-full unace # TODO: Check if all of these are necessary
 # Breeze Theme
 sudo apt install breeze-cursor-theme breeze-icon-theme
 # Fonts
 sudo apt install fonts-roboto fonts-firacode fonts-powerline ttf-mscorefonts-installer
-# zsh config
-## oh-my-zsh
+# oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 ## change default zsh
 chsh -s $(which zsh)
@@ -27,3 +24,5 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-
 sudo apt autoremove
 # Flatpak
 sudo apt update && sudo apt upgrade && sudo apt install flatpak && sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+# Optional
+sudo apt install lm-sensors coolercontrol nvtop ncdu mangohud
