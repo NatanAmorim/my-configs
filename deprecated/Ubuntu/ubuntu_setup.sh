cf68825984711d8ba2ef86fcecdda3ac99c470f5
Natan Amorim <39222731+NatanAmorim@users.noreply.github.com>
2025-07-13 02:43:56 -0300

    Update ubuntu_setup.sh

--- deprecated/Ubuntu/ubuntu_setup.sh
+++ deprecated/Ubuntu/ubuntu_setup.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 # ubuntu_setup.sh
 
-# You may need to "sudo chmod +x ubuntu_setup.sh"
+### This is my script for setting up a new Ubuntu install
+
+## You may need to "sudo chmod +x ubuntu_setup.sh"
 
 sudo apt update && sudo apt upgrade
-# Terminal workflow
+## Terminal workflow
 sudo apt install wget curl git zsh htop lsd neovim
-# Compilers & Interpreters
+## Compilers & Interpreters
 sudo apt install gcc make cmake build-essential autoconf automake
-# File compression (TODO: Check if these are necessary)
-# sudo apt install tar zip gzip unzip lrzip unrar p7zip-full unace
-# Breeze Theme
-sudo apt install breeze-cursor-theme breeze-icon-theme
-# Fonts
+## Fonts
 sudo apt install fonts-roboto fonts-firacode fonts-powerline ttf-mscorefonts-installer
-# oh-my-zsh
+## oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 ## change default zsh
 chsh -s $(which zsh)
 ## Syntax Highlights plugin
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-# Cleaning
+## Cleaning
 sudo apt autoremove
-# Flatpak
-sudo apt update && sudo apt upgrade && sudo apt install flatpak && sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-# Optional
-sudo apt install lm-sensors blueman coolercontrol nvtop ncdu mangohud
+## Flatpak
+sudo apt install flatpak && sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+### Optional
+# sudo apt install blueman mangohud nvtop ncdu btop gtop
+# sudo snap install bottom ## cli command: btm
+## Breeze Theme
+# sudo apt install breeze-cursor-theme breeze-icon-theme
